89a27cd2091e37787c847a36a984ba555b4739e1
saltcute <me@mizuno.ai>
2024-01-28 16:33:35 -0500

    fix: add real-ip

--- util/request.js
+++ util/request.js
@@ -44,7 +44,7 @@ const createRequest = (method, url, data = {}, options) => {
       headers['X-Real-IP'] = ip
       headers['X-Forwarded-For'] = ip
     }
-    // headers['X-Real-IP'] = '118.88.88.88'
+    headers['X-Real-IP'] = '47.120.8.222'
     if (typeof options.cookie === 'object') {
       options.cookie = {
         ...options.cookie,
